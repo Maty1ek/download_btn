340208e1712643187a8b868ceb5c1401be5105bb
Maty1ek <galymzakangerov@icloud.com>
2024-09-24 15:17:03 +0500

    created download btn

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const downloadBtn = document.querySelector('.download_btn')
 const filelink = "https://drive.google.com/uc?export=download&id=1lV9-HyDsOdjm8LK44HgsRi5s3cjB1G2J"
 
-function initTimer() {
-    let timer = downloadBtn.dataset.timer
-    downloadBtn.classList.add('timer')
-    downloadBtn.innerHTML = `your file will download in <b>${timer}</b> seconds`
+function downloadFunc() {
+    download.classList.add('timer')
+    download.innerHTML = `Your file will be downloaded in ${timer}`
 
-    const initCounter = setInterval(() => {
-        if(timer > 0) {
+    const counter = setInterval(() => {
+        if (timer > 0) {
             timer--
-            return downloadBtn.innerHTML = `your file will download in <b>${timer}</b> seconds`;
+            return download.innerHTML = `Your file will be downloaded in ${timer}`
         }
-        clearInterval(initCounter)
+        clearInterval(counter)
         location.href = filelink
-        downloadBtn.innerHTML = "Your file is downloading..."
+        download.innerHTML = `Your file is downloading`
 
         setTimeout(() => {
-            downloadBtn.classList.remove('timer')
-            downloadBtn.innerHTML = '<i class="fa-solid fa-download"></i><span class="text">Download Files</span>'
+            download.classList.remove('timer')
+            download.innerHTML = `<i class="fa-solid fa-download"></i>
+        <span class="text">Download Files</span>`
         }, 3000)
     }, 1000)
 }
 
-
-downloadBtn.addEventListener('click', initTimer)
+downloadBtn.addEventListener('click', downloadFunc)
